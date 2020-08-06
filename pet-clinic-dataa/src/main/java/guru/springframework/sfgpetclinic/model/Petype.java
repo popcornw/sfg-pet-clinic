b976b6341418a4b291aa4d016c82ae20a23bc79f
popcornw <46597178+popcornw@users.noreply.github.com>
2020-08-06 17:34:21 +0100

    added visits , updated owners relationship with pet

--- pet-clinic-dataa/src/main/java/guru/springframework/sfgpetclinic/model/Petype.java
+++ pet-clinic-dataa/src/main/java/guru/springframework/sfgpetclinic/model/Petype.java
@@ -12,3 +12,4 @@ public void setName(String name) {
 }
 
 }
+ 
---
+++ pet-clinic-dataa/src/main/java/guru/springframework/sfgpetclinic/model/Visit.java
@@ -0,0 +1,29 @@
+package guru.springframework.sfgpetclinic.model;
+
+import java.time.LocalDate;
+
+public class Visit extends BaseEntity {
+	private LocalDate date;
+	private String desctiprion;
+	private Pet pet;
+	public LocalDate getDate() {
+		return date;
+	}
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+	public String getDesctiprion() {
+		return desctiprion;
+	}
+	public void setDesctiprion(String desctiprion) {
+		this.desctiprion = desctiprion;
+	}
+	public Pet getPet() {
+		return pet;
+	}
+	public void setPet(Pet pet) {
+		this.pet = pet;
+	}
+	
+
+}
