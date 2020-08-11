74984fe293d950a9467fab2774b6fc691b970511
popcornw <46597178+popcornw@users.noreply.github.com>
2020-08-12 00:26:18 +0100

    updated owners with jpa entity mappings

--- pet-clinic-dataa/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-dataa/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -3,11 +3,22 @@ package guru.springframework.sfgpetclinic.model;
 import java.util.HashSet;
 import java.util.Set;
 
-public class Owner extends Person {
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+@Entity
+@Table(name="Owners")
 
+public class Owner extends Person {
+@Column(name="adress")
 	private String address;
+@Column(name="city")
 	private String city;
+@Column(name="telephone")
 	private String telephone;
+@OneToMany(cascade = CascadeType.ALL,mappedBy ="owner")
 	private Set<Pet> pets = new HashSet<>();
 
 	public String getAddress() {
